52c5d51c731853c4a07791d2f057e8855ce6716b
Sufairath C <sufairath.c@gmail.com>
2024-03-08 21:47:56 +0400

    updation

--- form-validation.js
+++ form-validation.js
@@ -1,49 +1,93 @@
 
+function fnameValidation(){
+   let fname = document.getElementById("fname-id")
+   let fnameError = document.getElementById("fname-error")
+   let regExp = /^[a-zA-Z]+$/
+   if(fname.value.trim() == '')
+   {
+      fnameError.innerHTML = "Full name cannot be blank"
+      return false
+   }
+   else if(!fname.value.match(regExp))
+   {
+      fnameError.innerHTML = "Full name must be in letters"
+      return false
+   }
+   else
+   {
+      fnameError.innerHTML = ''
+      return true
+   }
+}
 
-function handleSubmit(){
-    var message = document.getElementById('alert-id');
-    var successMsg = document.getElementById('success-id');
-    message.style.display = "none";
-    var nam = document.getElementById('fname-id').value;
-    var email = document.getElementById('email-id').value;
-    var pswd = document.getElementById('password-id').value;
-    var cpswd = document.getElementById('cpassword-id').value;
-    var submitBtn = document.getElementById('submit-id');
-    
-    
-       if(nam.length == 0 || /^[a-zA-Z]+$/.test(nam) == false)
-       {
-          message.innerText= "Please enter the name";
-          message.style.display = "block";
-          return false;
-       }
-    
-       if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) == false){
-          message.innerText = "Please enter the valid e-mail";
-          message.style.display = "block";
-          return false;
-       }
-    
-       if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(pswd) == false){
-          message.innerText = "Password should be 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character";
-          message.style.display = "block";
-          return false;
-       }
-    
-       if(pswd != cpswd)
-       {
-          message.innerText = "Passwords did nt match";
-          message.style.display = "block";
-          return false;
-       }
-       successMsg.style.display = "block";
-       successMsg.innerText = "Success!";
-       //submitBtn.addEventListener("load",home.html);
-       //submitBtn.onload(home.html);
-       //window.open("home.html");
-       //location.href("home.html");
-       //location.replace("./home.html");
-       
-       window.location.href = './home.html';
-       return false;
-    }
+function emailValidation(){
+   let email = document.getElementById("email-id")
+   let emailError = document.getElementById("email-error")
+   let regExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+   if(email.value.trim() == ''){
+      emailError.innerHTML = "email cannot be blank"
+      return false
+   }
+   else if(!email.value.match(regExp))
+   {
+      emailError.innerHTML = "Please enter an email correctly"
+      return false
+   }
+   else{
+      emailError.innerHTML = ''
+      return true
+   }
+}
+
+function pswdValidation(){
+   let pswd = document.getElementById("pswd-id")
+   let pswdError = document.getElementById("pswd-error")
+   let regExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+   if(pswd.value.trim() == ''){
+      pswdError.innerHTML = "Password cannot be blank"
+      return false;
+   }
+   else if(!pswd.value.match(regExp)){
+      pswdError.innerHTML = "Password should be 8 characters with atleast 1 uppercase, lowercase, numeric digit and special symbol"
+      return false
+   }
+   else{
+      pswdError.innerHTML = ''
+      return true
+   }
+}
+
+function cpswdValidation(){
+   let cpswd = document.getElementById('cpswd-id')
+   let cpswdError = document.getElementById('cpswd-error')
+   let pswd = document.getElementById('pswd-id')
+   if(cpswd.value.trim() == '')
+   {
+      cpswdError.innerHTML = "Password cannot be blank"
+      return false
+   }    
+   else if(pswd.value != cpswd.value){
+      cpswdError.innerHTML = "Password is not matched"
+      return false
+   }
+   else{
+      cpswdError.innerHTML = ''
+      return true
+   }
+}
+
+function submitValidation(){
+   let submitError = document.getElementById("submit-error")
+   if(!fnameValidation() || !emailValidation() || !pswdValidation() || !cpswdValidation()){
+      submitError.style.display = "flex"
+      submitError.style.justifyContent = "center"
+      submitError.style.marginLeft = "4%"
+      submitError.innerHTML = "ERROR"
+      return false
+   }
+   else{
+      submitError.style.display = "none"
+      submitError.innerHTML= ''
+      return true
+   }
+}
